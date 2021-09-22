3eb5bb53a7bcc3c6493369f6a178f11b150dc43c
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-09-22 17:23:27 +0200

    [feat] Game: score(): spare case

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,11 @@ public class Game {
 		int frame_number = 0;
 		for (int frame_counter = 0; frame_counter < 10; frame_counter++) {
 			int frame_score = rolls.get(frame_number) + rolls.get(frame_number + 1);
+			if (frame_score == 10) {
+				score+=(10 + rolls.get(frame_number + 2));
+			} else {
 				score+=frame_score;
+			}
 			frame_number+=2;
 		}
 		return score;
