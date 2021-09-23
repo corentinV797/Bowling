47b57811bcb3f2093d811b901500ade25ebb1fed
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-09-23 16:48:25 +0200

    [feat] Main: test cases examples

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		try {
+			Game g1 = new Game();
+			g1.read("X X X X X X X X X X X X");
+			System.out.println(g1.score());
+			
+			Game g2 = new Game();
+			g2.read("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-");
+			System.out.println(g2.score());
+			
+			Game g3 = new Game();
+			g3.read("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5");
+			System.out.println(g3.score());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
