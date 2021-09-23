0a98e866733c6390fa6d35b052222ba373c81cdd
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-09-23 23:55:47 +0200

    [feat] GameScorer: exception messages improved

--- src/GameScorer.java
+++ src/GameScorer.java
@@ -27,16 +27,16 @@ public class GameScorer {
 			frame_counter++;
 		}
 		if (frame_counter < Constants.FRAME_NUMBER) {
-			throw new NotEnoughFramesException("There isn't enough frame in this game");
+			throw new NotEnoughFramesException("There isn't enough frame in this game (number of frames = " + frame_counter + ")");
 		} else if (frame_counter > (Constants.FRAME_NUMBER + 2)) {
-			throw new TooManyFramesException("There are too many frames in this game");
+			throw new TooManyFramesException("There are too many frames in this game = " + frame_counter + ")");
 		}
 		
 		for (int i = 0; i < frame_counter; i++) {
 			if (isPotentialStrike(split[i])) {
 				char first_char = split[i].charAt(0);
 				if(!isStrikeChar(first_char)) {
-					throw new NotEnoughRollsException("Not enough roll in this frame");
+					throw new NotEnoughRollsException("Not enough roll in this frame (frame number = " + i + ")");
 				}
 				g.roll(Constants.STRIKE_SCORE);
 			} else if (isPotentialSpare(split[i])) {
@@ -44,13 +44,13 @@ public class GameScorer {
 				char second_char = split[i].charAt(1);
 				if (isSpareChar(second_char)) {
 					if (isStrikeChar(first_char) || isNoHitChar(first_char) || isSpareChar(first_char)) {
-						throw new InvalidFrameException("Invalid Frame");
+						throw new InvalidFrameException("Invalid Frame (frame number = " + i + ")");
 					}
 					readAndRoll(g, first_char);
 					g.roll(Constants.SPARE_SCORE - Character.getNumericValue(first_char));
 				} else {
 					if (isSpareChar(first_char) || isStrikeChar(first_char) || isStrikeChar(second_char)) {
-						throw new InvalidFrameException("Invalid Frame");
+						throw new InvalidFrameException("Invalid Frame (frame number = " + i + ")");
 					}
 					readAndRoll(g, first_char);
 					readAndRoll(g, second_char);
@@ -60,15 +60,15 @@ public class GameScorer {
 				char second_char = split[i].charAt(1);
 				char third_char = split[i].charAt(2);
 				if (i != (Constants.FRAME_NUMBER - 1)) {
-					throw new TooManyRollsException("Too many rolls in that frame");
+					throw new TooManyRollsException("Too many rolls in that frame (frame number = " + i + ")");
 				} else if (isSpareChar(first_char) || isStrikeChar(first_char) || !isSpareChar(second_char) || isSpareChar(third_char) || isStrikeChar(third_char)) {
-					throw new InvalidFrameException("Invalid Frame");
+					throw new InvalidFrameException("Invalid Frame (frame number = " + i + ")");
 				}
 				readAndRoll(g, first_char);
 				g.roll(Constants.SPARE_SCORE - Character.getNumericValue(first_char));
 				readAndRoll(g, third_char);
 			} else {
-				throw new TooManyRollsException("Too many rolls in that frame");
+				throw new TooManyRollsException("Too many rolls in that frame (" + split[i].length() + ")");
 			}
 		}
 	}
