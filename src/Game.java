a6ed86de0715b61dcd1794d12711ffaddb480f24
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-09-23 10:38:49 +0200

    [ref] Game: getNextFrameScore method

--- src/Game.java
+++ src/Game.java
@@ -7,7 +7,7 @@ public class Game {
 		int frame_number = 0;
 		for (int frame_counter = 0; frame_counter < 10; frame_counter++) {
 			if (rolls[frame_number] == 10) {
-				score+=(10 + rolls[frame_number + 1] + rolls[frame_number + 2]);
+				score+=(10 + getNextFrameScore(frame_number));
 				frame_number++;
 				continue;
 			} else if (frame_score == 10) {
@@ -26,5 +26,9 @@ public class Game {
 	
 	public int getCurrentFrameScore(int f) {
 		return rolls[f] + rolls[f + 1];
+	}
+	
+	public int getNextFrameScore(int f) {
+		return rolls[f + 1] + rolls[f + 2];	
 	}
 }
